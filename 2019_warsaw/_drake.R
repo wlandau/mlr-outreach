beac35979ebc95894dee0f998a1523ad4014e3b4
pat-s <patrick.schratz@gmail.com>
2019-07-31 15:50:42 +0200

    update

--- 2019_warsaw/_drake.R
+++ 2019_warsaw/_drake.R
@@ -15,8 +15,9 @@ benchmark_plan = code_to_plan("benchmark.R")
 
 # Set the config ----------------------------------------------------------
 
-drake_config(benchmark_plan, verbose = 2, parallelism = "clustermq", jobs = 2,
+drake_config(benchmark_plan, verbose = 2,# parallelism = "clustermq", #jobs = 2,
              #template = list(n_cpus = 5, memory = 34000, log_file = "worker%a.log"),
-             console_log_file = "drake.log")
+             #prework = quote(future::plan(future.callr::callr, workers = 4)),
+             console_log_file = "drake.log", lock_envir = F)
 
 
--- 2019_warsaw/benchmark.R
+++ 2019_warsaw/benchmark.R
@@ -5,7 +5,6 @@ learner = mlr_learners$mget(c("classif.kknn", "classif.svm"))
 measures = mlr_measures$mget("classif.ce")
 
 ## ParamSets
-
 param_set_svm = {
   foo = ParamSet$new(params = list(
     ParamDbl$new("cost", lower = -5, upper = 5),
@@ -17,10 +16,11 @@ param_set_svm = {
     x$gamma = 2^x$gamma
     return(x)
   }
+  return(foo)
 }
 
 param_set_knn = ParamSet$new(params = list(
-  ParamDbl$new("k", lower = 10, upper = 50),
+  ParamInt$new("k", lower = 10, upper = 50),
   ParamDbl$new("distance", lower = 1, upper = 50)
 ))
 
@@ -28,32 +28,27 @@ terminator = TerminatorEvaluations$new(100)
 resampling_inner = mlr_resamplings$get("cv3")
 
 # AutoTuner SVM
-
-## at_svm only contains "TRUE" instead of the R6 object :(
 at_svm = {
   foo = AutoTuner$new(learner$classif.svm, resampling_inner, measures = measures,
-                       param_set_svm, terminator, tuner = TunerRandomSearch)
-foo$store_bmr = TRUE
+                      param_set_svm, terminator, tuner = TunerRandomSearch)
+  foo$store_bmr = TRUE
+  return(foo)
 }
 
 # AutoTuner KNN
-
-# at_knn$store_bmr = FALSE :(
 at_knn = {
   foo = AutoTuner$new(learner$classif.kknn, resampling_inner, measures = measures,
                       param_set_knn, terminator, tuner = TunerRandomSearch)
-  #foo$store_bmr = TRUE
+  foo$store_bmr = TRUE
+  return(foo)
 }
-at_knn$store_bmr = TRUE
 
 # Resampling
 resampling_outer = mlr_resamplings$get("repeated_cv", param_vals = list(folds = 5, repeats = 10))
 
 # BM design
-design = expand_grid(tasks, list(at_svm, at_knn), resampling_outer)
+#design = expand_grid(tasks, list(at_svm, at_knn), resampling_outer)
+design = expand_grid(tasks, at_svm, resampling_outer)
 
 # execute benchmark
-bm = {
-  plan("multicore", workers = 5)
-  benchmark(design = design)
-}
+bm = benchmark(design = design)
